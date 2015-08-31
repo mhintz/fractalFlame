60a7ecbb90367087866771f54f4575f6fc1061bb
Mark Hintz <markohintz@gmail.com>
2015-08-31 23:25:49 +0200

    Change indentation

--- src/ofApp.h
+++ src/ofApp.h
@@ -5,42 +5,42 @@
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-		static int sideLength;
-		static int fieldStride;
-		static int fieldLength;
+	static int sideLength;
+	static int fieldStride;
+	static int fieldLength;
 
-		const int numSamples = 600000;
-		const int numTransforms = 3;
+	const int numSamples = 600000;
+	const int numTransforms = 3;
 
-		vector<float> field;
-		vector<TransformFunction> transforms;
-		vector<float> transformProbabilities;
+	vector<float> field;
+	vector<TransformFunction> transforms;
+	vector<float> transformProbabilities;
 
-		ofVec2f iterPosition;
-		ofVec3f color;
+	ofVec2f iterPosition;
+	ofVec3f color;
 
-		ofMesh thePoints = ofMesh();
+	ofMesh thePoints = ofMesh();
 
-		// Setting up the field of samples
-		Sample applyRandomTransform(const Sample &);
-		void resetField();
-		void generateField(ofVec2f, ofFloatColor);
-		ofVec2f getCoordinates(const ofVec2f &);
-		ofVec2f getPosFromIndex(int);
-		float scaleLocusAlpha(float);
+	// Setting up the field of samples
+	Sample applyRandomTransform(const Sample &);
+	void resetField();
+	void generateField(ofVec2f, ofFloatColor);
+	ofVec2f getCoordinates(const ofVec2f &);
+	ofVec2f getPosFromIndex(int);
+	float scaleLocusAlpha(float);
 };
